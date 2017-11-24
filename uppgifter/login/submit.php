2989b2d2bbf53abc71503af095a767692b018a7c
zinkdeveloper <rikard.viktig.nordahl@gmail.com>
2017-11-24 10:41:08 +0100

    nu kan man "logga in". den kollar om row är null eller inte

--- uppgifter/login/submit.php
+++ uppgifter/login/submit.php
@@ -6,4 +6,12 @@ $password = $_POST['password'];
 
 $sql = mysqli_query($dbc, "SELECT * FROM users WHERE mail = '$mail' AND password = '$password'");
 
+$row = mysqli_fetch_array($sql);
+
+if($row != null) {
+	echo "Successfully logged in!";
+} else {
+	echo "Fel mail eller lösenord, försök vänligen <a href='login.php'>här</a> igen!";
+}
+
 ?>
