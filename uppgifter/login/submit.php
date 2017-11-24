1fa5d125e0ff89ab1c9af43bff5ae9721a664ebb
zinkdeveloper <rikard.viktig.nordahl@gmail.com>
2017-11-24 10:55:46 +0100

    indentering

--- uppgifter/login/submit.php
+++ uppgifter/login/submit.php
@@ -3,18 +3,18 @@ $dbc = mysqli_connect("localhost", "root", "", "register");
 
 if(isset($_POST['mail']) && isset($_POST['password'])) { // funkar inte
 	
-$mail = htmlspecialchars($_POST['mail']);
-$password = htmlspecialchars($_POST['password']);	
+	$mail = htmlspecialchars($_POST['mail']);
+	$password = htmlspecialchars($_POST['password']);	
 
-$sql = mysqli_query($dbc, "SELECT * FROM users WHERE mail = '$mail' AND password = '$password'");
+	$sql = mysqli_query($dbc, "SELECT * FROM users WHERE mail = '$mail' AND password = '$password'");
 
-$row = mysqli_fetch_array($sql);
+	$row = mysqli_fetch_array($sql);
 
-if($row != null) {
-	echo "Successfully logged in!";
-} else {
-	echo "Fel mail eller lösenord, försök vänligen <a href='login.php'>här</a> igen!";
-}
+	if($row != null) {
+		echo "Successfully logged in!";
+	} else {
+		echo "Fel mail eller lösenord, försök vänligen <a href='login.php'>här</a> igen!";
+	}
 } else {	
 	echo "?";
 }
