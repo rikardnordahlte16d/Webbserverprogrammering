27d7681d27ca2ec9bdff79e7e79758715d34fc17
zinkdeveloper <rikard.viktig.nordahl@gmail.com>
2017-11-24 10:43:48 +0100

    la till htmlspecialchars för ökad säkerhet

--- uppgifter/login/submit.php
+++ uppgifter/login/submit.php
@@ -1,8 +1,8 @@
 <?php
 $dbc = mysqli_connect("localhost", "root", "", "register");
 
-$mail = $_POST['mail'];
-$password = $_POST['password'];	
+$mail = htmlspecialchars($_POST['mail']);
+$password = htmlspecialchars($_POST['password']);	
 
 $sql = mysqli_query($dbc, "SELECT * FROM users WHERE mail = '$mail' AND password = '$password'");
 
