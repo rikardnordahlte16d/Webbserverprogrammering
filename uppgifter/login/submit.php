a52b4ef582b51793498a62202705335a85cc3592
zinkdeveloper <rikard.viktig.nordahl@gmail.com>
2017-11-24 10:37:27 +0100

    hämtade all data där mailen och lösenordet är samma som det anvädaren skrev in.

--- uppgifter/login/submit.php
+++ uppgifter/login/submit.php
@@ -4,4 +4,6 @@ $dbc = mysqli_connect("localhost", "root", "", "register");
 $mail = $_POST['mail'];
 $password = $_POST['password'];	
 
+$sql = mysqli_query($dbc, "SELECT * FROM users WHERE mail = '$mail' AND password = '$password'");
+
 ?>
