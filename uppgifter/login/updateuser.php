f7ce6395bcd05b54df0243dbe40e962f2434f4f1
zinkdeveloper <rikard.viktig.nordahl@gmail.com>
2017-12-01 10:45:04 +0100

    kollar så att alla värden stämmer

--- uppgifter/login/updateuser.php
+++ uppgifter/login/updateuser.php
@@ -1,4 +1,10 @@
 <?php
+$dbc = mysqli_connect("localhost", "root", "", "register");
+session_start();
 
-
+if(isset($_POST['username']) && isset($_POST['full_name']) && isset($_POST['mail']) && isset($_POST['password']) && $_POST['username'] != "" && $_POST['username'] != "" && $_POST['full_name'] != "" && $_POST['mail'] != "" && $_POST['password']  != "" && isset($_SESSION['username'])) {
+	
+} else {	
+	header("Location:login.php");
+}
 ?>
