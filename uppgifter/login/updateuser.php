1c10b3ff684447e147fbbf2d7dccc607c7ab94ea
zinkdeveloper <rikard.viktig.nordahl@gmail.com>
2017-12-01 11:59:49 +0100

    htmlspecialchars i updateuser

--- uppgifter/login/updateuser.php
+++ uppgifter/login/updateuser.php
@@ -3,11 +3,11 @@ $dbc = mysqli_connect("localhost", "root", "", "register");
 session_start();
 
 if(isset($_POST['username']) && isset($_POST['full_name']) && isset($_POST['mail']) && isset($_POST['password']) && $_POST['username'] != "" && $_POST['username'] != "" && $_POST['full_name'] != "" && $_POST['mail'] != "" && $_POST['password']  != "" && isset($_SESSION['username'])) {
-	$username = $_POST['username'];
-	$full_name = $_POST['full_name'];
-	$mail = $_POST['mail'];
-	$date = $_POST['date'];
-	$password = $_POST['password'];
+	$username = htmlspecialchars($_POST['username']);
+	$full_name = htmlspecialchars($_POST['full_name']);
+	$mail = htmlspecialchars($_POST['mail']);
+	$date = htmlspecialchars($_POST['date']);
+	$password = htmlspecialchars($_POST['password']);
 	
 	if(!mysqli_query($dbc, "UPDATE users SET username = '$username', full_name = '$full_name', mail = '$mail', date = '$date', password = '$password' WHERE username = '" . $_SESSION['username'] . "';")) {
 		echo mysqli_error($dbc);
