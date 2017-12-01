ca62ea130eadc8318786b1a19a7c497435ebf079
zinkdeveloper <rikard.viktig.nordahl@gmail.com>
2017-12-01 10:48:56 +0100

    hämtar alla värden

--- uppgifter/login/updateuser.php
+++ uppgifter/login/updateuser.php
@@ -3,6 +3,11 @@ $dbc = mysqli_connect("localhost", "root", "", "register");
 session_start();
 
 if(isset($_POST['username']) && isset($_POST['full_name']) && isset($_POST['mail']) && isset($_POST['password']) && $_POST['username'] != "" && $_POST['username'] != "" && $_POST['full_name'] != "" && $_POST['mail'] != "" && $_POST['password']  != "" && isset($_SESSION['username'])) {
+	$username = $_POST['username'];
+	$full_name = $_POST['full_name'];
+	$mail = $_POST['mail'];
+	$date = $_POST['date'];
+	$password = $_POST['password'];
 	
 } else {	
 	header("Location:login.php");
