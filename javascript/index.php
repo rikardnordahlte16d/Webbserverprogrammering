ff92fffbade553f0a13b2c19aa1b9fd88a1f8e07
zinkdeveloper <rikard.viktig.nordahl@gmail.com>
2018-01-16 16:05:15 +0100

    lagt till att man kan få en alert,bild osv

--- javascript/index.php
+++ javascript/index.php
@@ -4,6 +4,36 @@
 	</head>
 	
 	<body>
-	
+		<button onmousedown="pressed()">Click</button>
+		<br><br>
+		<img onmouseover="bigger(this)" onmouseleave="smaller(this)" id="bild" src="bild.png" width=200 height=200>
+		<br><br>
+		<button onmousedown="hide()">Hide</button>
+		<button onmousedown="changeColor()">Change color</button>
+		<button onmousedown="clear()">Clear website</button>
+		<script>
+			function pressed() {
+				alert("Pressed!");
+			}
+			function bigger(image) {
+				image.style.height = "400px";
+				image.style.width = "400px";
+			}
+			function smaller(image) {
+				image.style.height = "200px";
+				image.style.width = "200px";
+			}
+			function hide() {
+				var image = document.getElementById("bild");
+				if(image.style.display == "none") {
+					image.style.display = "inline";
+				} else {
+					image.style.display = "none";
+				}
+			}
+			function changeColor() {
+				document.body.style.backgroundColor = "red";
+			}
+		</script>
 	</body>
 </html>
