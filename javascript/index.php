66cda15dce9b8ceeaef5718db38f284fda7c2c18
zinkdeveloper <rikard.viktig.nordahl@gmail.com>
2018-01-19 10:00:52 +0100

    indentering typ

--- javascript/index.php
+++ javascript/index.php
@@ -7,6 +7,11 @@
 			height:50px;
 			background-color:red;
 		}
+		#moveDiv {
+			width:50px;
+			height:50px;
+			background-color:blue;
+		}
 	</style>
 	</head>
 	
@@ -21,21 +26,56 @@
 			<button onmousedown="clearPage()">Clear website</button>
 			<br><br>
 			<div id="colorbox" onmousedown="changeColorBox()"></div>
-			<br><br>
 			
 			<div id="counter">
 				<p id="number">0</p>
 				<button onmousedown="increase(1)">1</button>
 				<button onmousedown="increase(10)">10</button>
 				<button onmousedown="increase(100)">100</button>
-				<br>
+				<br><br>
 				<button onmousedown="resetcounter()">Reset</button>
 			</div>
+			
+			<br><br>
+			<div id="moveDiv"></div>
+			<br>
+			<button onClick="move('up')">UP</button>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+			<button onClick="move('down')">DOWN</button>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+			<button onClick="move('right')">RIGHT</button>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+			<button onClick="move('left')">LEFT</button>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+			
 		</div>
 		<p id="cleartext"></p>
 		
 		<script>
 			var counter = 0;
+			
+			function move(dir){
+				element = document.getElementById("moveDiv");
+
+				if(dir == "up"){
+					margin = element.style.marginTop;
+					margin = margin.replace("px","");
+					element.style.marginTop = margin - 1;
+				}
+				if(dir == "down"){
+					margin = element.style.marginTop;
+					margin = margin.replace("px","");
+					element.style.marginTop = margin + 1;
+				}
+				if(dir == "left"){
+					margin = element.style.marginLeft;
+					margin = margin.replace("px","");
+					element.style.marginLeft = margin - 1;
+				}
+				if(dir == "right"){
+					margin = element.style.marginLeft;
+					margin = margin.replace("px","");
+					element.style.marginLeft = margin + 1;
+				}
+				
+			}
+			
 			function pressed() {
 				alert("Pressed!");
 			}
