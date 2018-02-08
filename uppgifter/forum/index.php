727d85a292e757f90e51c9102dd7ca3f86c2c8b7
zinkdeveloper <rikard.viktig.nordahl@gmail.com>
2018-02-08 12:17:10 +0100

    Förbereder login / register design

--- uppgifter/forum/index.php
+++ uppgifter/forum/index.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Titel</title>
+		<title>PWO Addicts</title>
 		<meta charset="UTF-8">
-		<style type="text/css">
-		</style>
+		<style type="text/css"></style>
 	</head>
 	<body>
+		<div id="login_buttons">
+			<button><a href="register.php">Registrera dig</a></button>
+			<button>Logga in</button>
+		</div>
+		<div id="login_field">
+			Användarnamn: <input type="text"/><br/>
+			Lösenord: <input type="password"/><br/>
+			<input type="checkbox"/> Kom ihåg mig &nbsp&nbsp&nbsp<a href="resetpassword.php">Glömt lösenordet?</a>
+		</div>
+		
 	</body>
 </html>
