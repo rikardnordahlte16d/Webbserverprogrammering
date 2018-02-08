b72c05b7e45d6bd0b109516ede024d50bd9f98e5
zinkdeveloper <rikard.viktig.nordahl@gmail.com>
2018-02-08 12:29:17 +0100

    bygger upp css och sånt

--- uppgifter/forum/index.php
+++ uppgifter/forum/index.php
@@ -3,14 +3,16 @@
 	<head>
 		<title>PWO Addicts</title>
 		<meta charset="UTF-8">
-		<style type="text/css"></style>
+		<link rel="stylesheet" href="css/index.css">
 	</head>
 	<body>
-		<div id="login_buttons">
-			<button><a href="register.php">Registrera dig</a></button>
-			<button>Logga in</button>
-		</div>
-		<div id="login_field">
+		<form>
+			<div id="login_buttons">
+				<button><a href="register.php">Registrera dig</a></button>
+				<button>Logga in</button>
+			</div>
+		</form>
+		<div id="login_field" style="display:none">
 			Användarnamn: <input type="text"/><br/>
 			Lösenord: <input type="password"/><br/>
 			<input type="checkbox"/> Kom ihåg mig &nbsp&nbsp&nbsp<a href="resetpassword.php">Glömt lösenordet?</a>
